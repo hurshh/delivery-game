d442ea00067893da125836fcd7061b061e2e7b88
HARSH RAO <quasarhrsh@gmail.com>
2021-12-18 22:33:04 +0530

    game completed

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -1,20 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+    [SerializeField] Color32 hasPackageColor = new Color32(255, 255, 255, 1);
+    [SerializeField] Color32 NoPackageColor = new Color32(255, 255, 255, 1);
+    private SpriteRenderer spriterendered;
+
+    private void Start()
+    {
+        spriterendered = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Package"))
+        if (other.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Package Picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, 0.5f);
+            spriterendered.color = hasPackageColor;
+
         }
 
-        if (other.CompareTag("Boost"))
+        if (other.CompareTag("Boost") && hasPackage)
         {
             Debug.Log("Boost Activated");
+            hasPackage = false;
+            Destroy(other.gameObject, 0.5f);
+            spriterendered.color = NoPackageColor;
         }
+        
     }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,25 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Windows;
+
 
 public class Driver : MonoBehaviour
 {
     
     
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    float moveSpeed = 10f;
+    float fastSpeed = 20f;
+    float slowSpeed = 5f;
 
     // Update is called once per frame
     void Update()
     {
         float steerAmount = UnityEngine.Input.GetAxis("Horizontal")* Time.deltaTime*150;
-        float steerSpeed = UnityEngine.Input.GetAxis("Vertical")* Time.deltaTime*10;
+        float steerSpeed = UnityEngine.Input.GetAxis("Vertical")* Time.deltaTime*moveSpeed;
         
         transform.Rotate(0, 0, steerAmount*-1);
         transform.Translate(0, steerSpeed, 0);
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Boost") )
+        {
+            Debug.Log("Boost Activated");
+            moveSpeed = fastSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
 }
